d2430fbf9b4c0774e104a73be3b83008eb3f2ca6
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-12-12 14:31:20 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+price = 10
+rating = 4.9
+name = 'Abbas'
+is_published = False
+isNot_published = True
+print(price)
+full_name = 'John Smith'
+age = 20
+is_newPatient = True
+if(is_newPatient):
+    print("Welcome to our clinic, Mr", full_name)
+name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + favorite_color)
