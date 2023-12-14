54a03ee6f3584ceee3e4e16aa6bd7f6104d7a697
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-12-14 04:03:07 +0300

    add if statements and operators code examples

---
+++ if_statements.py
@@ -0,0 +1,18 @@
+is_rainy = False
+is_sunny = False
+if is_rainy:
+    print('Take your umbrella.')
+elif is_sunny:
+    print('Take your sun glasses.')
+else:
+    print("It's a lovely day.")
+
+print('Enjoy your day.')
+
+house_price = 1000000
+has_good_credit = False
+if has_good_credit:
+    down_payment =  house_price * 0.1
+else: 
+    down_payment =  house_price * 0.2
+print(f"Down payment: ${down_payment}")
---
+++ operators.py
@@ -0,0 +1,20 @@
+import math
+print(math.ceil(2.4))
+
+print(math.floor(2.4))
+#operator precedence
+#1 parenthesis
+#2 exponentiation
+#3 multiplication and division (from left to right)
+#4 addition and subtraction (from left to right)
+x = (10 + 3) * 2 ** 2
+print(x)
+y = (2+3) * 10 - 3
+print(y)
+
+
+x = 3.9
+print(round(x))
+
+
+print(abs(-1.3))
