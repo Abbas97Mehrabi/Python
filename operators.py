7a0a79177ab30004392bb2cea5dd94d468bad6c1
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-12-15 01:25:30 +0300

    adding car game, guessing program and loops code examples

--- operators.py
+++ operators.py
@@ -18,3 +18,33 @@ print(round(x))
 
 
 print(abs(-1.3))
+
+#logical operators
+has_high_income = False
+has_good_credit = True
+has_criminal_record = False
+#AND: both
+#OR: at least one
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+
+
+#comparison operators
+
+temperature = 35
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+name = "mahmut"
+
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name can be a macimum of 50 characters")
+else:
+    print("Name looks good!")
--- pound_to_kg.py
+++ pound_to_kg.py
@@ -1,3 +1,14 @@
-weight_lbs = input("insert your weight (lbs) : ")
-kg = int(float(weight_lbs) * 453.59237 / 1000)
-print ("Your Weight is ", kg, "Kilograms")
+weight = input("Weight : ")
+unit = input("(L)bs or K(g): ")
+kg_to_lbs = int(float(weight) * 2.20462262185)
+lbs_to_kg = int(float(weight) * 453.59237 / 1000)
+
+if unit.lower() == "k":
+    print("Your weight is ", kg_to_lbs, " lbs" )
+
+elif unit.lower() == "l":
+    print ("Your Weight is ", lbs_to_kg, "Kilograms")
+else:
+    print("Invalid Input! Please enter 'L' for Pounds or 'K' for Kilograms.")
+
+
