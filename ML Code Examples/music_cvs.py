5771fb0bad1e28377e50fef51a8c9789151332ca
Abbas <124402065+AbbasMehrabi97@users.noreply.github.com>
2023-12-22 02:21:49 +0300

    ML code example

---
+++ ML Code Examples/music_cvs.py
@@ -0,0 +1,18 @@
+# import pandas as pd
+# from sklearn.tree import DecisionTreeClassifier as dtc
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score
+# # from joblib import dump, load
+
+# music_data = pd.read_csv('music.csv')
+# X = music_data.drop(columns=['genre'])
+# Y = music_data['genre']
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# model = dtc()
+# model.fit(X_train, Y_train)
+# predictions = model.predict(X_test)
+
+# # model = load('music-recommender.joblib')
+# score = accuracy_score(Y_test, predictions)
+# score
